a83dfac8b15804dce08ed6ce8d2e406ff1909545
tomstao <96sutao@gmail.com>
2024-11-01 13:02:59 -0400

    pokedex

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <list>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace std;
+    map<string,list<string>> pokedex;
+
+    list<string> pikachuAttacks{"thunder shock", "tail whip", "quick attack"};
+    list<string>  charmanderAttacks{"flame thrower", "scary face"};
+    list<string>  chikoritaAttacks{"razor leaf", "poison powder"};
+
+    pokedex.insert(pair<string,list<string> >("Pikachu", pikachuAttacks));
+    pokedex.insert(pair<string,list<string> >("Charmander", charmanderAttacks));
+    pokedex.insert(pair<string,list<string> >("Chikorita", chikoritaAttacks));
+
+    for(auto pair : pokedex)
+    {
+        cout << pair.first << " - ";
+        for(auto attack : pair.second)
+        {
+            cout << attack << endl;
+        }
+    }
+
     return 0;
 }
